dba7f6efe24926bb6ebfa424484ab23e89f8f950
teiyou416 <130667154+teiyou416@users.noreply.github.com>
2023-06-07 17:00:17 +0000

    w

--- intro/js/app.js
+++ intro/js/app.js
@@ -23,13 +23,13 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#27374D"
       },
       "shape": {
         "type": "circle",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#27374D"
         },
         "polygon": {
           "nb_sides": 5
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#ffffff",
+        "color": "#27374D",
         "opacity": 0.4,
         "width": 1
       },
